1f464eee5083eb9a3235526528fd35e203d75a75
JesusIslam <omicron.b3ta@gmail.com>
2017-01-23 13:28:03 +0700

    fixing old thread deletion handler

--- custommiddleware/delete_old_thread.go
+++ custommiddleware/delete_old_thread.go
@@ -14,14 +14,60 @@ func DeleteOldThread(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) (err error) {
 		resp := &response.Response{}
 
-		db := database.New()
+		tx := database.New().Begin()
 
-		yesterday := time.Now().Add(-24 * time.Hour)
-		err = db.Table("threads").Where("created_at < ?", yesterday).Unscoped().Delete(&model.Post{}).Error
-		if err != nil && err != database.NotFound {
+		// get all posts
+		posts := []*model.Post{}
+		err = tx.Table("posts").Find(&posts).Error
+		if err != nil {
+			tx.Rollback()
 			resp.Error = err.Error()
 			return c.JSON(http.StatusServiceUnavailable, resp)
 		}
+
+		// get last post of each thread
+		// thread_id : post
+		threadPostMap := map[int64]*model.Post{}
+		for _, post := range posts {
+			// if already exists, check if created_at is newer
+			// if newer, replace
+			if lastPost := threadPostMap[post.ThreadID]; lastPost != nil {
+				if lastPost.CreatedAt.Before(post.CreatedAt) {
+					threadPostMap[post.ThreadID] = post
+				}
+			} else {
+				threadPostMap[post.ThreadID] = post
+			}
+		}
+
+		// from each thread, check which has last post more than 24h ago
+		threadsToBeDeleted := []int64{}
+		yesterday := time.Now().Add(-24 * time.Hour)
+		for threadID, post := range threadPostMap {
+			if post.CreatedAt.Before(yesterday) {
+				threadsToBeDeleted = append(threadsToBeDeleted, threadID)
+			}
+		}
+
+		if len(threadsToBeDeleted) > 0 {
+			// delete all of those threads
+			err = tx.Table("threads").Where("id IN (?)", threadsToBeDeleted).Unscoped().Delete(&model.Thread{}).Error
+			if err != nil && err != database.NotFound {
+				tx.Rollback()
+				resp.Error = err.Error()
+				return c.JSON(http.StatusServiceUnavailable, resp)
+			}
+
+			// delete all posts of those threads
+			err = tx.Table("posts").Where("thread_id in (?)", threadsToBeDeleted).Unscoped().Delete(&model.Post{}).Error
+			if err != nil && err != database.NotFound {
+				tx.Rollback()
+				resp.Error = err.Error()
+				return c.JSON(http.StatusServiceUnavailable, resp)
+			}
+		}
+
+		tx.Commit()
 
 		return next(c)
 	}
